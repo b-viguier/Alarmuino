e61404756214ceff4a8c37648bc19bfab1dfe8d8
Benoit Viguier <benoit.viguier.pro+github@gmail.com>
2018-01-03 13:57:36 +0100

    Fixed warnings for arduino compiler

--- src/Core/Property.h
+++ src/Core/Property.h
@@ -3,6 +3,11 @@
 
 namespace Core {
 
+    /**
+     * Get/Set value of a property
+     *
+     * @tparam T Type of the handled property
+     */
     template<typename T>
     class Property {
     public:
@@ -12,43 +17,67 @@ namespace Core {
 
         virtual void set(Type value) = 0;
 
-        template<class C>
-        class WrapperReadOnly : public Property {
-        public:
-            typedef Type (C::*GetFunction)() const;
-
-            WrapperReadOnly(C &instance, GetFunction getter)
-                    : _instance(instance), _getter(getter) {}
-
-            Type get() const final {
-                return (_instance.*_getter)();
-            }
-
-            void set(Type value) override {
-            }
-
-        protected:
-            C &_instance;
-            GetFunction _getter;
-        };
-
-        template<class C>
-        class Wrapper : public WrapperReadOnly<C> {
-        public:
-            typedef void (C::*SetFunction)(Type);
-
-            Wrapper(C &instance, typename WrapperReadOnly<C>::GetFunction getter, SetFunction setter)
-                    : WrapperReadOnly<C>(instance, getter), _setter(setter) {}
-
-            void set(Type value) final {
-                (this->_instance.*_setter)(value);
-            }
-
-        protected:
-            SetFunction _setter;
-        };
+        template<class C> class WrapperReadOnly;
+        template<class C> class Wrapper;
     };
 
+
+    /**
+     * Read-only property of an object
+     *
+     * @tparam T Type of the handled property
+     * @tparam C Object owner of the property
+     */
+    template<typename T>
+    template<class C>
+    class Property<T>::WrapperReadOnly : public Property<T> {
+    public:
+        typedef Type (C::*GetFunction)() const;
+
+        WrapperReadOnly(C &instance, GetFunction getter)
+                : _instance(instance), _getter(getter) {}
+
+        Type get() const final {
+            return (_instance.*_getter)();
+        }
+
+        void set(Type value) final {
+        }
+
+    private:
+        C &_instance;
+        GetFunction _getter;
+    };
+
+    /**
+     * Read/Write property of an object
+     *
+     * @tparam T Type of the handled property
+     * @tparam C Object owner of the property
+     */
+    template<typename T>
+    template<class C>
+    class Property<T>::Wrapper : public Property<T> {
+    public:
+        typedef Type (C::*GetFunction)() const;
+        typedef void (C::*SetFunction)(Type);
+
+        Wrapper(C &instance, GetFunction getter, SetFunction setter)
+                : _instance(instance), _getter(getter), _setter(setter) {}
+
+        Type get() const final {
+            return (_instance.*_getter)();
+        }
+
+        void set(Type value) final {
+            return (_instance.*_setter)(value);
+        }
+
+    private:
+        C &_instance;
+        GetFunction _getter;
+        SetFunction _setter;
+    };
 }
 
 #endif //ALARMUINO_PROPERTY_H
