3ba7d56b1c72f30a77f7830d09ace491adfe0aed
Benoit Viguier <benoit.viguier.pro+github@gmail.com>
2018-01-08 13:17:23 +0100

    Added ArrayFixedSize

--- src/Utils/Array.h
+++ src/Utils/Array.h
@@ -10,12 +10,37 @@ namespace Utils {
      */
     template<typename T>
     class Array {
-
+    public:
         virtual unsigned int size() const = 0;
 
         virtual T &operator[](unsigned int i) = 0;
 
         virtual const T &operator[](unsigned int i) const = 0;
+    };
+
+    template<typename T, unsigned short Size>
+    class ArrayFixedSize : public Array<T> {
+    public:
+
+        template<typename ...Args>
+        explicit ArrayFixedSize(Args &...args) : _buffer{&args...} {
+            static_assert(sizeof...(args) == Size, "Number of arguments doesn't match size of the array");
+        }
+
+        inline T &operator[](unsigned int i) final {
+            return *_buffer[i];
+        }
+
+        inline const T &operator[](unsigned int i) const final {
+            return *_buffer[i];
+        }
+
+        inline unsigned int size() const final {
+            return Size;
+        }
+
+    private:
+        T *_buffer[Size];
     };
 
 /**
@@ -27,7 +52,7 @@ namespace Utils {
  * @tparam Capacity Maximum number of elements.
  */
     template<typename T, unsigned short Capacity>
-    class ArrayFixedCapacity : Array<T> {
+    class ArrayFixedCapacity : public Array<T> {
     public:
 
         ArrayFixedCapacity()
